4f74e52dcc226a09fe6fb764531142abcfbe5aa2
a-rosillo <128966633+a-rosillo@users.noreply.github.com>
2023-03-29 18:23:49 +0100

    Random number generator and random sentence generator added.'

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-const sentenceComponetns = {
+const sentenceComponents = {
     person: ['You', 'Someone', 'Everyone'],
     action: ['give', 'desire', 'have', 'reject', 'mourn for', 'long for'],
     thing: ['love', 'everything', 'somthing special']
 }
 
-console.log(`${sentenceComponetns.person[0]} will ${sentenceComponetns.action[0]} ${sentenceComponetns.thing[0]}.`)
+const randomNumber = maxValue => Math.floor(Math.random()*(maxValue+1))
+
+const randomSentence = () => {
+    const person = sentenceComponents.person[randomNumber(sentenceComponents.person.length-1)]
+    const action = sentenceComponents.action[randomNumber(sentenceComponents.action.length-1)]
+    const thing = sentenceComponents.thing[randomNumber(sentenceComponents.thing.length-1)]
+
+    console.log(`${person} will ${action} ${thing}.`)
+}
+
+randomSentence()
