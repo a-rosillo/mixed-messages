07e50258acf54b130bff56d9bf199b743cce0a49
a-rosillo <128966633+a-rosillo@users.noreply.github.com>
2023-03-29 18:12:38 +0100

    Sentence components added and tested with console.log().

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('hello')
+const sentenceComponetns = {
+    person: ['You', 'Someone', 'Everyone'],
+    action: ['give', 'desire', 'have', 'reject', 'mourn for', 'long for'],
+    thing: ['love', 'everything', 'somthing special']
+}
+
+console.log(`${sentenceComponetns.person[0]} will ${sentenceComponetns.action[0]} ${sentenceComponetns.thing[0]}.`)
